e0d8ce58c87fc9db63c734246339597de95491ef
Quentin Lee <151835338+ktqlee@users.noreply.github.com>
2024-07-11 16:12:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Error Message String
+const error_required = "This field is required";
+const invalid_email = "Please enter a valid email address";
+const error_required_query = "Please select a query type";
+const error_required_consent = "To submit this form, please consent to being contacted";
