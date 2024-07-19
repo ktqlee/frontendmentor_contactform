c976d46e4b4ed915763e842b2631f852e9a0765a
Quentin Lee <151835338+ktqlee@users.noreply.github.com>
2024-07-19 18:00:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const error_required = "This field is required";
 const invalid_email = "Please enter a valid email address";
 const error_required_query = "Please select a query type";
 const error_required_consent = "To submit this form, please consent to being contacted";
+
+const form = document.getElementById("form")
+
+function validateform(){
+    let sumbitcondition = false;
+
+    let firstname = document.getElementById("firstname");
+    let lastname = document.getElementById("lastname");
+    let email = document.getElementById("email");
+
+    return sumbitcondition;
+}
